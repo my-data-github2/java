67de5867137d4d22c69c841203d591cf82a58b5c
kmark15 <kunal15markam@gmail.com>
2023-10-19 23:50:26 +0530

    Assignment-11_1 Completed

---
+++ Assignment-11/Assignment-11_1/src/com/sunbeam/Main.java
@@ -0,0 +1,28 @@
+package com.sunbeam;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Set<Book> set = new HashSet<>();
+	    set.add(new Book("1","Gate","Kunal",1,20.0));  
+	    set.add(new Book("4","Rate","Rahul",2,50.0)); 
+	    set.add(new Book("5","Mate","Somesh",1,60.0)); 
+	    set.add(new Book("3","Date","Aman",4,10.0)); 
+	    set.add(new Book("2","Late","Shivansh",3,20.0)); 
+	    set.add(new Book("5","Sate","Vijay",5,90.0)); 
+	    
+	    for(Book b: set) {
+	    	System.out.println(b);
+	    }
+	}
+
+}
+
+
+// If any duplicate isbn is added then the duplicate value is not allowed, as doesn't contain any duplicate value in it. 
+// the first occured isbn allowed only, and duplicate one is not allowed. 
+
+// books are based on hashcode.
